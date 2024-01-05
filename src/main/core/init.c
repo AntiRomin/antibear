93e4f9f1833fe8f2fa6f35246f93176d6ca01e83
Romin Chen <413196205@qq.com>
2024-01-05 15:34:56 +0800

    Removed flash example code

--- src/main/core/init.c
+++ src/main/core/init.c
@@ -17,9 +17,6 @@
 
 uint8_t systemState = SYSTEM_STATE_INITIALISING;
 
-uint8_t writeData[16384];
-uint8_t readData[16384] = {0};
-
 void init(void)
 {
     systemInit();
@@ -29,54 +26,6 @@ void init(void)
     flashInit();
 
     debugInit();
-
-    const flashGeometry_t *flashGeometry = flashGetGeometry(FLASH_ID_W25Q256JV);
-
-    uint32_t flashSectorSize = flashGeometry->sectorSize;
-    uint32_t flashPageSize = flashGeometry->pageSize;
-
-    uint32_t flashStartAddress = 0;
-
-    uint32_t bytesRemaining = 16384;
-    uint32_t offset = 0;
-
-    for (uint32_t i = 0; i < 16384; i++) {
-        writeData[i] = 6;
-    }
-
-    do {
-        uint32_t flashAddress = flashStartAddress + offset;
-
-        uint32_t bytesToWrite = bytesRemaining;
-        if (bytesToWrite > flashPageSize) {
-            bytesToWrite = flashPageSize;
-        }
-
-        bool onSectorBoundary = flashAddress % flashSectorSize == 0;
-        if (onSectorBoundary) {
-            flashEraseSector(FLASH_ID_W25Q256JV, flashAddress);
-        }
-
-        flashPageProgram(FLASH_ID_W25Q256JV, flashAddress, (uint8_t *)&writeData[offset], bytesToWrite, NULL);
-
-        bytesRemaining -= bytesToWrite;
-        offset += bytesToWrite;
-    } while (bytesRemaining > 0);
-
-    uint32_t totalBytesRead = 0;
-    int bytesRead = 0;
-
-    bool success = false;
-
-    do {
-        bytesRead = flashReadBytes(FLASH_ID_W25Q256JV, flashStartAddress + totalBytesRead, &readData[totalBytesRead], 16384 - totalBytesRead);
-        if (bytesRead > 0) {
-            totalBytesRead += bytesRead;
-            success = (totalBytesRead == 16384);
-        }
-    } while (!success && bytesRead > 0);
-
-    flashWaitForReady(FLASH_ID_W25Q256JV);
 
     systemState |= SYSTEM_STATE_READY;
 }
